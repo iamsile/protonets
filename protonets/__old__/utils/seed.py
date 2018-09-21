970acacd5dd044f844e6a3532b6973059028bd6d
filangel <filos.angel@gmail.com>
2018-09-21 18:49:14 +0300

    remove protonets/__old__ folder

--- protonets/__old__/utils/seed.py
+++
@@ -1,8 +0,0 @@
-def set_seed(seed: int):
-    """Inititialize random generators seed."""
-    import torch
-    import numpy as np
-    torch.cuda.cudnn_enabled = False
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
